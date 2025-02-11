302f10fa0d542b6fe3a54be847ebff161177c6d1
Faridhsbi <mhmmd.faridhsbi@gmail.com>
2025-02-11 19:03:09 +0700

    fix: fix typo in createproductpost method

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -25,7 +25,7 @@ public class ProductController {
     @PostMapping( "/create")
     public String createProductPost(@ModelAttribute Product product, Model model){
         service.create(product);
-        return "redirect:/list";
+        return "redirect:list";
     }
 
     @GetMapping("/list")
--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -7,5 +7,5 @@ import lombok.Setter;
 public class Product {
     private String productId;
     private String productName;
-    private double productQuantity;
+    private int productQuantity;
 }
