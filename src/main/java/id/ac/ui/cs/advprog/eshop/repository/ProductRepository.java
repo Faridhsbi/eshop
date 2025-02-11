31502e611f91cea553dbecd74e7a1c83d584860d
Faridhsbi <mhmmd.faridhsbi@gmail.com>
2025-02-11 21:48:04 +0700

    delete product implementetion

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -7,16 +7,33 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
     public Product create(Product product) {
+        // Generate UUID untuk productId jika belum ada
+        if (product.getProductId() == null || product.getProductId().isEmpty()) {
+            product.setProductId(UUID.randomUUID().toString());
+        }
         productData.add(product);
         return product;
     }
+    
     public Iterator<Product> findAll() {
         return productData.iterator();
     }
+
+    public void delete(Product product) {
+        if (product != null && product.getProductId() != null) {
+            productData.removeIf(p ->
+                    product.getProductId().equals(p.getProductId())
+            );
+        }
+    }
+
+
+
 }
